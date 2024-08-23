130a55afe14ccadc5b05bb15ae82166cd82fbd48
Owais Khan <kowais915@gmail.com>
2024-08-23 09:48:19 +0500

    ✅ Landing page completed
    Hero section + a list of tasks.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
-
+import Landing from "@/components/pages/Landing";
+import TaskList from "@/components/ui/TaskList";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div>This is the test</div>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+        <Landing/>
+        <div className="mb-[40px] w-full">
+          <TaskList/>
+        </div>
     </main>
   );
 }
